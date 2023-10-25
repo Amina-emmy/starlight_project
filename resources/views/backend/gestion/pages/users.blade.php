d9b3b6c783d015dd5e7eb3f6a065b9ea3bc3cbd6
Amina-emmy <amina.barghoute2001@gmail.com>
2023-10-25 15:41:06 +0100

    simple mod

--- resources/views/backend/gestion/pages/users.blade.php
+++ resources/views/backend/gestion/pages/users.blade.php
@@ -1,7 +1,21 @@
 @include('layouts.index_admin')
 <div class="main-content">
-    @include('backend.gestion.partials.userInfo')
-
+    <div class="contenir">
+        <div class="header-title">
+            <h4>Users</h4>
+        </div>
+    
+        <div class="user-info">
+            <div class="img">
+                <a href="{{ route('profile.edit') }}">
+                    <img src={{ asset('storage/images_users/' . auth()->user()->image) }} alt="" srcset="">
+                </a>
+            </div>
+            <a href="{{ route('profile.edit') }}" class="nav-link">
+                <p>{{ auth()->user()->name }}</p>
+            </a>
+        </div>
+    </div>
     <div class="py-3 container">
         <h1>Jury Members :</h1>
         @include('backend.gestion.partials.users.tableJurys')
